99c33721fa18cd6d48765fbd2ddbaa230360d44f
Ayush <imayush19052003@gmail.com>
2025-07-28 19:35:07 +0530

    BASE_URL dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,7 @@
-// export const  BASE_URL = "http://localhost:3000";
-export const  BASE_URL = "/api";
+//  export const  BASE_URL = "http://localhost:3000";
+//export const  BASE_URL = "/api";
+
+export const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "/api";
